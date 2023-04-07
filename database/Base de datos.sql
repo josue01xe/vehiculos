b565057277ed001d0ee04f635b1f46dec239dbfb
Josue <josuebuleje@gmail.com>
2023-04-07 09:58:08 -0500

    BD registrar

--- database/Base de datos.sql
+++ database/Base de datos.sql
@@ -6,7 +6,7 @@ CREATE TABLE vehiculos
 idvehiculo INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 marca          VARCHAR(80)     NOT NULL,
 modelo         VARCHAR(80)     NOT NULL,
-año            DATE            NOT NULL,
+año            YEAR            NOT NULL,
 tipocombustible VARCHAR(50)    NOT NULL,
 color           VARCHAR(50)    NOT NULL,
 numeroplaca    CHAR(6)         NOT NULL,
@@ -18,8 +18,8 @@ fechacompra     DATE           NOT NULL
 )ENGINE = INNODB;
 
 INSERT INTO vehiculos(marca, modelo, año, tipocombustible, color, numeroplaca, transmision, kilometraje, tipovehiculo, fechacompra) VALUES
-('toyota', 'corolla', '2016-05-15', 'gasolina', 'rojo', '123456', 'A', 150.000, 'automovil', '2022-10-20'),
-('ford', 'focus', '2018-04-10', 'diesel', 'negro', 'AEF777', 'M', 100.000, 'automovil', '2023-01-10');
+('toyota', 'corolla', '2017', 'gasolina', 'rojo', '123456', 'A', 150.000, 'automovil', '2022-10-20'),
+('ford', 'focus', '2018', 'diesel', 'negro', 'AEF777', 'M', 100.000, 'automovil', '2023-01-10');
 
 SELECT * FROM vehiculos;
 
@@ -42,4 +42,24 @@ BEGIN
 END $$
 
  CALL spu_vehiculos_listar();
-       
+ 
+DELIMITER $$
+CREATE PROCEDURE spu_vehiculos_registrar
+(
+  IN _marca           VARCHAR(80),
+  IN _modelo          VARCHAR(80),
+  IN _año             YEAR,
+  IN _tipocombustible VARCHAR(50),
+  IN _color           VARCHAR(50),
+  IN _numeroplaca     CHAR(6),
+  IN _transmision     CHAR(1),
+  IN _kilometraje     DECIMAL(7,3),
+  IN _tipovehiculo    VARCHAR(70),
+  IN _fechacompra     DATE
+)
+BEGIN
+  INSERT INTO vehiculos (marca, modelo, año, tipocombustible, color, numeroplaca, transmision, kilometraje, tipovehiculo, fechacompra) VALUES
+  (_marca, _modelo, _año, _tipocombustible, _color, _numeroplaca, _transmision, _kilometraje, _tipovehiculo, _fechacompra);
+  END $$
+  
+  CALL spu_vehiculos_registrar('tesla','cybertruck','2020','electrico','negro','987654','A','500.000','camioneta','2022-05-22')
